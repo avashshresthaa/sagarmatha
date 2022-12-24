fe46c2b0e25e59bc315519c1a8f59699aaba95ca
avashsth9 <avashsth@gmail.com>
2022-12-24 16:57:33 +0545

    Login Page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_4/pages/home_page.dart';
+import 'package:flutter_application_4/services/networkhelper.dart';
+
+import '../models/loginmodel.dart';
+import 'home_page.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -9,117 +12,180 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   bool isVisible = true;
 
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView(
-          padding: EdgeInsets.symmetric(
-            vertical: 80,
-            horizontal: 16,
-          ),
-          children: [
-            Image.asset(
-              'assets/logo.png',
-              width: 147,
-              height: 135,
+        child: Form(
+          key: _formkey,
+          child: ListView(
+            padding: EdgeInsets.symmetric(
+              vertical: 80,
+              horizontal: 16,
             ),
-            Center(
-              child: Text(
-                'Log in to continue',
-                style: TextStyle(
-                  color: Colors.green,
-                  fontSize: 13,
-                  fontWeight: FontWeight.w700,
-                ),
+            children: [
+              Image.asset(
+                'assets/logo.png',
+                width: 147,
+                height: 135,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Username',
-                prefixIcon: Icon(
-                  Icons.person,
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12),
-                  ),
-                  borderSide: BorderSide(
+              Center(
+                child: Text(
+                  'Log in to continue',
+                  style: TextStyle(
                     color: Colors.green,
-                    width: 2,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12),
-                  ),
-                  borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 2,
+                    fontSize: 13,
+                    fontWeight: FontWeight.w700,
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              obscureText: isVisible,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                prefixIcon: Icon(
-                  Icons.person,
-                ),
-                suffixIcon: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isVisible = !isVisible;
-                    });
-                  },
-                  child: Icon(
-                    isVisible ? Icons.visibility : Icons.visibility_off,
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: phoneController,
+                decoration: InputDecoration(
+                  hintText: 'Phone',
+                  prefixIcon: Icon(
+                    Icons.person,
                   ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
                   ),
-                  borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 2,
-                  ),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12),
-                  ),
-                  borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 2,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 50,
+              SizedBox(
+                height: 20,
               ),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return HomePage();
-                  }));
+              TextFormField(
+                controller: passwordController,
+                obscureText: isVisible,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter your password';
+                  }
                 },
-                child: Text('Login'),
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: Icon(
+                    Icons.person,
+                  ),
+                  suffixIcon: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isVisible = !isVisible;
+                      });
+                    },
+                    child: Icon(
+                      isVisible ? Icons.visibility : Icons.visibility_off,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 2,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                      width: 2,
+                    ),
+                  ),
+                ),
               ),
-            )
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50,
+                ),
+                child: ElevatedButton(
+                  onPressed: () async {
+                    var phoneData = phoneController.text.toString();
+                    var passwordData = passwordController.text.toString();
+                    //9849203704
+                    //Nepal@12344
+                    if (_formkey.currentState!.validate()) {
+                      Login logindata = await NetworkHelper().getLogin(
+                        phone: phoneData,
+                        password: passwordData,
+                      );
+
+                      var status = logindata.status;
+                      print('ERROR: $status');
+
+                      if (status == 'success') {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return HomePage();
+                        }));
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return Expanded(
+                              child: AlertDialog(
+                                title: Text('Error'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text('Ok'),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        );
+                        //print('error');
+
+                      }
+                    } else {
+                      print('error');
+                    }
+                  },
+                  child: Text('Login'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/services/networkhelper.dart
+++ lib/services/networkhelper.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter_application_4/models/getposts.dart';
+import 'package:flutter_application_4/models/loginmodel.dart';
 import 'package:http/http.dart' as http;
 
 class NetworkHelper {
@@ -11,5 +15,33 @@ class NetworkHelper {
       var body = response.body;
       return postsFromJson(body);
     }
+  }
+
+  Future<Login> getLogin({
+    var phone,
+    var password,
+  }) async {
+    var client = http.Client();
+    var loginmodel;
+
+    var uri = Uri.parse(
+        'http://103.38.197.18/mobilebanking/api/v2/mdabaliReportUserLogin');
+    var response = await client.post(
+      uri,
+      headers: {HttpHeaders.contentTypeHeader: "application/json"},
+      body: jsonEncode(
+        <dynamic, dynamic>{
+          "mobileno": phone,
+          "password": password,
+        },
+      ),
+    );
+
+    if (response.statusCode == 200) {
+      var body = response.body;
+      var jsonMap = jsonDecode(body);
+      loginmodel = Login.fromJson(jsonMap);
+    }
+    return loginmodel;
   }
 }
